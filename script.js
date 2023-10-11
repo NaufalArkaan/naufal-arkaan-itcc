580a482374ee8f27846e3d1061905f1f5b459343
NaufalArkaan <naufalarkaan25@gmail.com>
2023-10-11 17:40:06 +0800

    upload projek

---
+++ script.js
@@ -0,0 +1,14 @@
+// ALERT PEMBUKAAN
+alert("Welcome To My Website");
+
+// PENGATURAN LOAD
+window.addEventListener('load', function () {
+    document.querySelector('body').classList.add("loaded")
+});
+
+// PENGATURAN SEND CONTACT
+function send() {
+    if (onclick = "btn") {
+        alert("Your Answer Has Been Sent")
+    }
+}
